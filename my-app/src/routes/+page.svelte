087db6d7a309eb159054c0554eba1e870f2cd88a
Sarah Carlat <sncarlat@gmail.com>
2024-03-31 11:30:49 -0500

    Svelte-kit HW

--- my-app/src/routes/+page.svelte
+++ my-app/src/routes/+page.svelte
@@ -1,2 +1,64 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<html> 
+    <p>
+        You can use <abbr>LOL</abbr> (League of Legends) to reference a soul-sucking game that causes many symptoms like anger and depression.
+        <div></div>
+        <style>
+            p {
+                color:  #26b72b;
+            }
+        </style>
+        
+        To talk more about <b class="term">League of Legends</b> you can contact: 
+    </p>
+    <address> <a href="support@riotgames.com">support@riotgames.com</a></address>
+    <div></div>
+    <h1>Is there a change you will enjoy playing League of Legends? Consult the image below:</h1>
+    <map name="LOL infographic"
+        <area
+            shape="poly"
+            coords="129,0,260,95,129,138"
+            href="https://images.squarespace-cdn.com/content/56968843a12f44a0077af789/1455229441374-RJ69X0V0LPAG0MPOQH96/no-300x225.jpg?content-type=image%2Fjpeg"
+            target="_blank"
+            alt="Do You Enjoy Having Fun?" />
+          <area
+            shape="poly"
+            coords="260,96,209,249,130,138"
+            href="https://images.squarespace-cdn.com/content/56968843a12f44a0077af789/1455229441374-RJ69X0V0LPAG0MPOQH96/no-300x225.jpg?content-type=image%2Fjpeg"
+            target="_blank"
+            alt="Are You Currently Happy?" />
+          <area
+            shape="poly"
+            coords="209,249,49,249,130,139"
+            href="https://images.squarespace-cdn.com/content/56968843a12f44a0077af789/1455229441374-RJ69X0V0LPAG0MPOQH96/no-300x225.jpg?content-type=image%2Fjpeg"
+            target="_blank"
+            alt="Have You Had A Good Life?" />
+          <area
+            shape="poly"
+            coords="48,249,0,96,129,138"
+            href="https://images.squarespace-cdn.com/content/56968843a12f44a0077af789/1455229441374-RJ69X0V0LPAG0MPOQH96/no-300x225.jpg?content-type=image%2Fjpeg"
+            target="_blank"
+            alt="Do You Have Loved Ones?" />
+          <area
+            shape="poly"
+            coords="0,95,128,0,128,137"
+            href="https://images.squarespace-cdn.com/content/56968843a12f44a0077af789/1455229441374-RJ69X0V0LPAG0MPOQH96/no-300x225.jpg?content-type=image%2Fjpeg"
+            target="_blank"
+            alt="Do You Think It'll Be a Fun Game?" />
+        </map>
+        <img usemap="#infographic" src="/media/examples/mdn-info.png" alt="MDN infographic" />
+        <figure>
+            <figcaption>Still Want To Play League? Listen to this:</figcaption>
+            <audio controls src="c:\Users\Sarah\Downloads\Voice - Sion - New 2014 Voice.mp3"></audio>
+        </figure>
+<div></div>
+<blockquote cite="https://www.metacritic.com/game/league-of-legends/user-reviews/">
+    <p>Piece of crap game that caters only to the paying bunch. Let's face it, you pay, you get good champs. 
+        You do not, you get crap champs. Overall, a crappy game for crappy individuals
+        Not to mention the difference in power for some. Take brand or that **** lee sin...spam 1 skill, win the match. 
+        Simple logic from the riot developer ****.</p>
+    <footer>—Hypogramos, <cite>Brave League of Legends Player</cite></footer>
+  </blockquote>
+
+</html>
