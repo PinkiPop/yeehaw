19195cfe757162e1fa177c5a695ebc8bd8cc4e87
Sarah Carlat <sncarlat@gmail.com>
2024-04-07 15:18:58 -0500

    added javascript, changed divs to buttons, added a header

--- my-app/src/routes/+page.svelte
+++ my-app/src/routes/+page.svelte
@@ -2,35 +2,62 @@
 <link rel="stylesheet" href="/src/routes/Soundboard.css">
 </head>
 <body>
-  <div class="First"> 
-    <div class="button1">Button 1</div>
-    <div class="button1">Button 2</div>
-    <div class="button1">Button 3</div>
-    <div class="button1">Button 4</div>
-    <div class="button1">Button 5</div>
-    <div class="button2">Button 6</div>
-    <div class="button2">Button 7</div>
+<script src="/home/scarlat/projects/yeehaw/my-app/src/routes/addingsound/+page.js"></script>
+<div class="Header">
+  <h1>Best League of Legends Voice Lines</h1><br>
+</div> 
+<div class="First">
+ 
+
+    <button class="button1" id="button-1">Button 2</button>
+    <button class="button1" id="button-2">Button 3</button>
+    <button class="button1 "id="button-3">Button 4</button>
+    <button class="button1" id="button-4">Button 5</button>
+    <button class="button1 "id="button-5">Button 6</button>
+    <button class="button1 "id="button-6">Button 7</button>
   </div>
 
   <div class="Second"> 
-    <div class="button3">Button 1</div>
-    <div class="button4">Button 2</div>
-    <div class="button4">Button 3</div>
-    <div class="button5">Button 4</div>
-    <div class="button5">Button 5</div>
-    <div class="button6">Button 6</div>
+    <button class="button1" id="button-7">Button 1</button>
+    <button class="button1" id="button-8">Button 2</button>
+    <button class="button1"id="button-9">Button 3</button>
+    <button class="button1"id="button-10">Button 4</button>
+    <button class="button1"id="button-11">Button 5</button>
+    <button class="button1"id="button-12">Button 6</button>
   </div>
 
   <div class="Third"> 
-    <div class="button7">Button 1</div>
-    <div class="button7">Button 2</div>
-    <div class="button7">Button 3</div>
-    <div class="button7">Button 4</div>
-    <div class="button7">Button 5</div>
-    <div class="button7">Button 6</div>
-    <div class="button7">Button 7</div>
-    <div class="button7">Button 8</div>
-    <div class="button6">Button 9</div>
+    <button class="button1"id="button-13">Button 1</button>
+    <button class="button1"id="button-14">Button 2</button>
+    <button class="button1"id="button-15">Button 3</button>
+    <button class="button1"id="button-16">Button 4</button>
+    <button class="button1"id="button-17">Button 5</button>
+    <button class="button1"id="button-18">Button 6</button>
+    <button class="button1"id="button-19">Button 7</button>
+    <button class="button1"id="button-20">Button 8</button>
+    <button class="button1"id="button-21">Button 9</button>
   </div>
+
+  <audio id="audio-1" src="/home/scarlat/projects/yeehaw/my-app/src/routes/ok-rammus.mp3"></audio>
+  <audio id="audio-2" src=""></audio>
+  <audio id="audio-3" src=""></audio>
+  <audio id="audio-4" src=""></audio>
+  <audio id="audio-5" src=""></audio>
+  <audio id="audio-6" src=""></audio>
+  <audio id="audio-7" src=""></audio>
+  <audio id="audio-8" src=""></audio>
+  <audio id="audio-9" src=""></audio>
+  <audio id="audio-10" src=""></audio>
+  <audio id="audio-11" src=""></audio>
+  <audio id="audio-12" src=""></audio>
+  <audio id="audio-13" src=""></audio>
+  <audio id="audio-14" src=""></audio>
+  <audio id="audio-15" src=""></audio>
+  <audio id="audio-16" src=""></audio>
+  <audio id="audio-17" src=""></audio>
+  <audio id="audio-18" src=""></audio>
+  <audio id="audio-19" src=""></audio>
+  <audio id="audio-20" src=""></audio>
+  <audio id="audio-21" src=""></audio>
 
 </body>
---
+++ my-app/src/routes/addingsound/+page.js
@@ -0,0 +1,133 @@
+
+  const soundButton1 = document.getElementById('button-1');
+  const audio1 = document.getElementById('audio-1');
+  soundButton1.addEventListener('click', function() {
+    audio1.play();
+    soundButton1.addEventListener('click', function() {
+      if (audio1.paused) {
+          audio1.play();
+      } else {
+          audio1.pause();
+      }
+});
+
+  const soundButton2 = document.getElementById('button-2');
+  const audio2 = document.getElementById('audio-2');
+  soundButton2.addEventListener('click', function() {
+    audio2.play();
+});
+
+  const soundButton3 = document.getElementById('button-3');
+  const audio3 = document.getElementById('audio-3');
+  soundButton3.addEventListener('click', function() {
+    audio3.play();
+});
+
+  const soundButton4 = document.getElementById('button-4');
+  const audio4 = document.getElementById('audio-4');
+  soundButton4.addEventListener('click', function() {
+    audio4.play();
+});
+
+  const soundButton5 = document.getElementById('button-5');
+  const audio5 = document.getElementById('audio-5');
+  soundButton5.addEventListener('click', function() {
+    audio5.play();
+});
+
+  const soundButton6 = document.getElementById('button-6');
+  const audio6 = document.getElementById('audio-6');
+  soundButton6.addEventListener('click', function() {
+    audio6.play();
+});
+
+  const soundButton7 = document.getElementById('button-7');
+  const audio7 = document.getElementById('audio-7');
+  soundButton7.addEventListener('click', function() {
+    audio7.play();
+});
+
+  const soundButton8 = document.getElementById('button-8');
+  const audio8 = document.getElementById('audio-8');
+  soundButton8.addEventListener('click', function() {
+    audio8.play();
+});
+
+  const soundButton9 = document.getElementById('button-9');
+  const audio9 = document.getElementById('audio-9');
+  soundButton9.addEventListener('click', function() {
+    audio9.play();
+});
+
+  const soundButton10 = document.getElementById('button-10');
+  const audio10 = document.getElementById('audio-10');
+  soundButton10.addEventListener('click', function() {
+    audio10.play();
+});
+
+  const soundButton11 = document.getElementById('button-11');
+  const audio11 = document.getElementById('audio-11');
+  soundButton11.addEventListener('click', function() {
+    audio11.play();
+});
+
+  const soundButton12 = document.getElementById('button-12');
+  const audio12 = document.getElementById('audio-12');
+  soundButton12.addEventListener('click', function() {
+    audio12.play();
+});
+
+  const soundButton13 = document.getElementById('button-13');
+  const audio13 = document.getElementById('audio-13');
+  soundButton13.addEventListener('click', function() {
+    audio13.play();
+});
+
+  const soundButton14 = document.getElementById('button-14');
+  const audio14 = document.getElementById('audio-14');
+  soundButton14.addEventListener('click', function() {
+    audio14.play();
+});
+
+  const soundButton15 = document.getElementById('button-15');
+  const audio15 = document.getElementById('audio-15');
+  soundButton15.addEventListener('click', function() {
+    audio15.play();
+});
+
+  const soundButton16 = document.getElementById('button-16');
+  const audio16 = document.getElementById('audio-16');
+
+  soundButton16.addEventListener('click', function() {
+    audio16.play();
+});
+
+  const soundButton17 = document.getElementById('button-17');
+  const audio17 = document.getElementById('audio-17');
+  soundButton17.addEventListener('click', function() {
+    audio17.play();
+});
+
+  const soundButton18 = document.getElementById('button-18');
+  const audio18 = document.getElementById('audio-18');
+  soundButton18.addEventListener('click', function() {
+    audio18.play();
+});
+
+  const soundButton19 = document.getElementById('button-19');
+  const audio19 = document.getElementById('audio-19');
+  soundButton19.addEventListener('click', function() {
+    audio19.play();
+});
+
+  const soundButton20 = document.getElementById('button-20');
+  const audio20 = document.getElementById('audio-20');
+  soundButton20.addEventListener('click', function() {
+    audio20.play();
+});
+
+  const soundButton21 = document.getElementById('button-21');
+  const audio21 = document.getElementById('audio-21');
+  soundButton21.addEventListener('click', function() {
+    audio21.play();
+});})
