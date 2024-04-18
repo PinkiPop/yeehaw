e737dfe53a9ba256a8d9d551b6eb91c28608fe24
Sarah Carlat <sncarlat@gmail.com>
2024-04-18 18:09:51 -0500

    began phaser game

---
+++ my-app/src/lib/sbbutton.svelte
@@ -0,0 +1,16 @@
+<script>
+    export let name;
+    export let audiosrc;
+</script>
+<div>
+    <button>
+{name}
+    </button>
+    <audio src="{audiosrc}"></audio>
+</div>
+<style>
+    button{
+        width: 120px;
+        
+    }
+</style>
